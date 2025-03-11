cca130c760fb2960a1e553c92732bb8556a0cca6
praveen-kantubotu <kantubotupraveenkumar@gmail.com>
2025-03-11 20:52:32 +0530

    Update provider.tf

--- infra/base/provider.tf
+++ infra/base/provider.tf
@@ -1,6 +1,6 @@
 # Setup our aws provider
 variable "region" {
-  default = "eu-west-1"
+  default = "eu-west-1"  # make it as a variable so no need to modify when to deploy to multiple regions
 }
 provider "aws" {
   region = "${var.region}"
